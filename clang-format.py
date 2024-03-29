1fdd21f201d282aa13128323dec6cc60ab906b70
zhaosx <shaoxi2010@qq.com>
2024-03-29 18:35:36 +0800

    添加clang-format脚本

---
+++ clang-format.py
@@ -0,0 +1,44 @@
+# 用clang-format格式化文件，通过在文件夹下添加.formatignore文件
+# 可以将同级别目录下的对应文件夹或文件忽略
+from subprocess import check_call
+from os import path, walk
+from concurrent.futures import ThreadPoolExecutor
+
+mutithreads = 8
+
+pwd = path.abspath(path.dirname(__file__))
+
+suffixs = ['.c', '.h', '.cpp', '.hpp']
+
+def run_clang_fmt(files):
+    for file in files:
+        print(f"Formatting {file}")
+        check_call(["clang-format", "-i", file])
+
+
+def split_list(lst, n):
+    return [lst[i : i + n] for i in range(0, len(lst), n)]
+
+
+def formatignore(ignorefile):
+    with open(ignorefile, 'r') as f:
+        return f.read().splitlines()
+
+if __name__ == "__main__":
+    file_list = []
+    for root, dirs, files in walk(pwd):
+        if '.formatignore' in files:
+            for entry in formatignore(path.join(root, '.formatignore')):
+                if path.isdir(path.join(root, entry)):
+                    dirs.remove(entry)
+                else:
+                    files.remove(entry)
+        for file in files:
+            if file.endswith(tuple(suffixs)):
+                file_list.append(path.join(root, file))
+
+    print("Starting format using mutithread")
+    for file_chunk in split_list(file_list, 10):
+        pool = ThreadPoolExecutor(max_workers=mutithreads)   
+        pool.submit(run_clang_fmt, file_list)
+    print("format completed")
